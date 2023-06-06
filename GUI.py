497bf21c1f22eb1f5f31382bd965c38e21e070aa
KGatewayB <94070857+KGatewayB@users.noreply.github.com>
2023-06-06 18:40:28 +0200

    Add files via upload

---
+++ GUI.py
@@ -0,0 +1,65 @@
+import sys
+import http.server
+import socketserver
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+import threading
+import os
+import socket
+
+PORT = 8000
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+class HttpServerThread(threading.Thread):
+    def run(self):
+        # Create socket
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server.bind(('127.0.0.1', PORT))
+        server.listen(1)
+        print("V Test 1 passed")
+        
+        while True:    
+            # Wait for client connections
+            client_connection, client_address = server.accept()
+            # Get the client request
+            request = client_connection.recv(1024).decode()
+            # Get the content of calc.html
+            html = open(dir_path+'/dir/calc.html')
+            content = html.read()
+            html.close()
+
+            # Send HTTP response
+            # IP of the client print(client_address[0])
+            response = 'HTTP/1.0 200 OK\n\n' + content
+            client_connection.sendall(response.encode())
+            client_connection.close()
+            break
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(100, 100, 420, 590)
+        self.setWindowIcon(QIcon(dir_path+'/dir/icon.ico'))
+        self.setWindowTitle("Calculator by K.G.B")
+        self.webview = QWebEngineView()
+        self.setCentralWidget(self.webview)
+        self.load_page()
+        print("V Test 3 passed")
+
+    def load_page(self):
+        url = QUrl("http://localhost:{}/calc.html".format(PORT))
+        self.webview.load(url)
+        print("V Test 2 passed")
+
+if __name__ == "__main__":
+    # Start the HTTP server in a separate thread
+    http_server = HttpServerThread()
+    http_server.start()
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+        
